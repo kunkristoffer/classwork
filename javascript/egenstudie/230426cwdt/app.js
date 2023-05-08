aafc02f112db28d8e944f0395e49682ba16fe896
kristofferfh <kristofferfh.kodehode@gmail.com>
2023-05-08 11:32:18 +0200

    prettified code

--- javascript/egenstudie/230426cwdt/app.js
+++ javascript/egenstudie/230426cwdt/app.js
@@ -7,7 +7,12 @@ const debug = document.getElementById("debug");
 input.addEventListener("input", updateValue);
 
 // Table of cast when damage taken data
-const cwdt = [528,583,661,725,812,897,1003,1107,1221,1354,1485,1635,1804,1980,2184,2394,2621,2874,3142,3272,3580,3950,4350,4780,5240,5730,6250,6800,7380,7990,8310,8630,8965,9300,9650,10000,10365,10730,11110,11490];
+const cwdt = [
+  528, 583, 661, 725, 812, 897, 1003, 1107, 1221, 1354, 1485, 1635, 1804, 1980,
+  2184, 2394, 2621, 2874, 3142, 3272, 3580, 3950, 4350, 4780, 5240, 5730, 6250,
+  6800, 7380, 7990, 8310, 8630, 8965, 9300, 9650, 10000, 10365, 10730, 11110,
+  11490,
+];
 
 function updateValue() {
   // initialization of input values
@@ -19,37 +24,44 @@ function updateValue() {
   const inputDivergent = document.getElementById("inputDivergent").value;
   const inputHeartbound = document.getElementById("inputHeartbound").checked;
   const inputSkeleton = document.getElementById("inputSkeleton").checked;
-  const inputForbiddenrite = document.getElementById("inputForbiddenrite").checked;
+  const inputForbiddenrite =
+    document.getElementById("inputForbiddenrite").checked;
 
   // calculations
-  const heartboundDamage = inputHeartbound ? 840 : 420 ;
-  const skeletonDamage = inputSkeleton ? heartboundDamage * 4 : heartboundDamage * 3;
-  const forbiddenriteDamage = inputForbiddenrite ? ((inputLife * 0.4) + (inputEnergy * 0.25)) * (1-(inputChaos/100)) : 0;
-  const totalDamage = inputCharacter ? (skeletonDamage + forbiddenriteDamage) * 2 : (skeletonDamage + forbiddenriteDamage);
+  const heartboundDamage = inputHeartbound ? 840 : 420;
+  const skeletonDamage = inputSkeleton
+    ? heartboundDamage * 4
+    : heartboundDamage * 3;
+  const forbiddenriteDamage = inputForbiddenrite
+    ? (inputLife * 0.4 + inputEnergy * 0.25) * (1 - inputChaos / 100)
+    : 0;
+  const totalDamage = inputCharacter
+    ? (skeletonDamage + forbiddenriteDamage) * 2
+    : skeletonDamage + forbiddenriteDamage;
 
   // printing cwdt table to html, but first clear the area
-  while(output.firstChild)
-  output.removeChild(output.firstChild);
+  while (output.firstChild) output.removeChild(output.firstChild);
 
   // Warns if you're taking chaos damage
   if (forbiddenriteDamage - inputWard > 0) {
-    debug.textContent = `${Math.round(forbiddenriteDamage - inputWard)} damage taken`;
+    debug.textContent = `${Math.round(
+      forbiddenriteDamage - inputWard
+    )} dmg taken to life`;
   } else {
     // had to include else statement to clear debug if no damage will be taken
     debug.textContent = ``;
   }
-    
 
   // Lets generate that table with some nice colors
   for (let i = 0; i < cwdt.length; i++) {
     // Generate values for calc
-    const innLevel = i+1;
+    const innLevel = i + 1;
     const innValue = cwdt[i];
-    const innDivergent = Math.floor(cwdt[i]-(cwdt[i] * 20 / 100));
-    const innCustom = Math.floor(cwdt[i]-(cwdt[i] * inputDivergent / 100));
+    const innDivergent = Math.floor(cwdt[i] - (cwdt[i] * 20) / 100);
+    const innCustom = Math.floor(cwdt[i] - (cwdt[i] * inputDivergent) / 100);
     const innMin = Math.min(innValue, innDivergent, innCustom);
 
-    // Starts a fragment
+    // Using fragment to combine all changes before pushing to html
     const fragment = document.createDocumentFragment();
 
     // Generates divs for html
@@ -64,20 +76,17 @@ function updateValue() {
     outDivergent.textContent = innDivergent;
     outCustom.textContent = innCustom;
 
-    // color green if bellow threshold 
-    if (innMin*0.9 < totalDamage && innMin*1.5 > totalDamage) {
-      fragment.appendChild(outLevel);
-      fragment.appendChild(outValue);
-      fragment.appendChild(outDivergent);
-      fragment.appendChild(outCustom);
-      
-      if ((innValue||innDivergent||innCustom) < totalDamage) 
+    // color green if bellow threshold
+    if (innMin * 0.9 < totalDamage && innMin * 1.5 > totalDamage) {
+      fragment.append(outLevel, outValue, outDivergent, outCustom);
+
+      if ((innValue || innDivergent || innCustom) < totalDamage)
         fragment.appendChild(outLevel).setAttribute("class", "green");
-      if (innValue < totalDamage) 
+      if (innValue < totalDamage)
         fragment.appendChild(outValue).setAttribute("class", "green");
-      if (innDivergent < totalDamage) 
+      if (innDivergent < totalDamage)
         fragment.appendChild(outDivergent).setAttribute("class", "green");
-      if (innCustom < totalDamage) 
+      if (innCustom < totalDamage)
         fragment.appendChild(outCustom).setAttribute("class", "green");
     }
 
@@ -86,4 +95,4 @@ function updateValue() {
     // levels are tied to the correct values
     output.prepend(fragment);
   }
-};
+}

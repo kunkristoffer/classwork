307cfeaf77088ff211dc97c8b92cdafa68627df3
kristofferfh <kristofferfh.kodehode@gmail.com>
2023-05-08 14:07:32 +0200

    updated version of cwdt

--- javascriptAdvanced/egenstudie/230426cwdt/app.js
+++ javascriptAdvanced/egenstudie/230426cwdt/app.js
@@ -1,51 +1,43 @@
 // defining input and output areas
 const input = document.querySelector("fieldset");
 const output = document.getElementById("cwdt-table");
-const debug = document.getElementById("debug");
+const debugField = document.getElementById("debug");
+const inputs = (div, type) => document.getElementById(div)[type];
 
 // adds eventlistener that checks for user inputs
 input.addEventListener("input", updateValue);
 
 // Table of cast when damage taken data
-const cwdt = [
-  528, 583, 661, 725, 812, 897, 1003, 1107, 1221, 1354, 1485, 1635, 1804, 1980,
-  2184, 2394, 2621, 2874, 3142, 3272, 3580, 3950, 4350, 4780, 5240, 5730, 6250,
-  6800, 7380, 7990, 8310, 8630, 8965, 9300, 9650, 10000, 10365, 10730, 11110,
-  11490,
+const cwdt = [528, 583, 661, 725, 812, 897, 1003, 1107, 1221, 1354, 1485, 1635, 1804, 1980, 2184, 2394, 2621, 2874, 3142, 3272, 3580, 3950, 4350, 4780, 5240, 5730, 6250, 6800, 7380, 7990, 8310, 8630, 8965, 9300, 9650, 10000, 10365, 10730, 11110, 11490,
 ];
 
+// Updates values and prints table
 function updateValue() {
-  // initialization of input values
-  const inputCharacter = document.getElementById("saboteur").checked;
-  const inputLife = document.getElementById("inputLife").value;
-  const inputEnergy = document.getElementById("inputEnergy").value;
-  const inputWard = document.getElementById("inputWard").value;
-  const inputChaos = document.getElementById("inputChaos").value;
-  const inputDivergent = document.getElementById("inputDivergent").value;
-  const inputHeartbound = document.getElementById("inputHeartbound").checked;
-  const inputSkeleton = document.getElementById("inputSkeleton").checked;
-  const inputForbiddenrite =
-    document.getElementById("inputForbiddenrite").checked;
+  // fetches character stats from input fields
+  const stats = {
+    character: inputs("saboteur", "checked"),
+    life: inputs("inputLife", "value"),
+    energy: inputs("inputEnergy", "value"),
+    ward: inputs("inputWard", "value"),
+    chaos: inputs("inputChaos", "value"),
+    diver: inputs("inputDivergent", "value"),
+    heart: inputs("inputHeartbound", "checked") ? 840 : 420,
+    skeleton: inputs("inputSkeleton", "checked") ? 4 : 3,
+    rite: inputs("inputForbiddenrite", "checked"),
+  }
 
   // calculations
-  const heartboundDamage = inputHeartbound ? 840 : 420;
-  const skeletonDamage = inputSkeleton
-    ? heartboundDamage * 4
-    : heartboundDamage * 3;
-  const forbiddenriteDamage = inputForbiddenrite
-    ? (inputLife * 0.4 + inputEnergy * 0.25) * (1 - inputChaos / 100)
-    : 0;
-  const totalDamage = inputCharacter
-    ? (skeletonDamage + forbiddenriteDamage) * 2
-    : skeletonDamage + forbiddenriteDamage;
+  const skelDamage = stats.skeleton * stats.heart;
+  const forbDamage = stats.rite ? (stats.life * 0.4 + stats.energy * 0.25) * (1 - stats.chaos / 100) : 0;
+  const totalDamage = stats.character ? (skelDamage + forbDamage) * 2 : skelDamage + forbDamage;
 
   // printing cwdt table to html, but first clear the area
   while (output.firstChild) output.removeChild(output.firstChild);
 
   // Warns if you're taking chaos damage
-  if (forbiddenriteDamage - inputWard > 0) {
+  if (forbDamage - stats.ward > 0) {
     debug.textContent = `${Math.round(
-      forbiddenriteDamage - inputWard
+      forbDamage - stats.ward
     )} dmg taken to life`;
   } else {
     // had to include else statement to clear debug if no damage will be taken
@@ -58,7 +50,7 @@ function updateValue() {
     const innLevel = i + 1;
     const innValue = cwdt[i];
     const innDivergent = Math.floor(cwdt[i] - (cwdt[i] * 20) / 100);
-    const innCustom = Math.floor(cwdt[i] - (cwdt[i] * inputDivergent) / 100);
+    const innCustom = Math.floor(cwdt[i] - (cwdt[i] * stats.diver) / 100);
     const innMin = Math.min(innValue, innDivergent, innCustom);
 
     // Using fragment to combine all changes before pushing to html

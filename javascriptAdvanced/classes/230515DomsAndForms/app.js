c7fc9c853aa0f64211efce9c1de744ac506b8782
kristofferfh <kristofferfh.kodehode@gmail.com>
2023-05-15 11:37:01 +0200

    uploading classworks

---
+++ javascriptAdvanced/classes/230515DomsAndForms/app.js
@@ -0,0 +1,56 @@
+const outPut = document.getElementById("output")
+const buttonSubmit = document.getElementById("input-submit")
+const formE1 = document.getElementById("input-form")
+const formText = document.getElementById("input-text")
+const createDiv = document.createElement("div")
+const createOl = document.createElement("ol")
+
+// function handleButton() {
+//     const paraE1 = document.querySelector("p")
+//     const inputE1 = document.getElementById("#text-input")
+//     const checkE1 = document.getElementById("#checkbox-input")
+//     outPut.textContent = inputE1.value
+// }
+// buttonSubmit.addEventListener("click", handleButton)
+
+/* function handleForm(event) {
+    const outputLI = document.createElement("li")
+
+    const inputValues = formText.textContent
+
+    outputLI.textContent = inputValues
+    
+    outPut.appendChild(outputLI)
+} */
+
+function addTodo(event) {
+    // når du bruker submit event sammen med form så må man bruke 
+    // event.preventDefault() fra å reloade siden
+    event.preventDefault()
+
+    const createLi = document.createElement("li")
+    const createP = document.createElement("p")
+    const createCheckbox = document.createElement("input");
+    const createBtn = document.createElement("button");
+    createCheckbox.setAttribute("type", "checkbox");
+
+    createBtn.textContent = "-"
+    createP.textContent = formText.value
+
+    // removes todo entry on click
+    // https://stackoverflow.com/questions/46665554/remove-parent-element-on-click-with-plain-javascript
+    createBtn.addEventListener("click", (e) => {
+        e.target.parentNode.remove();
+    })
+
+
+
+
+    createLi.append(createCheckbox,createP,createBtn)
+    outPut.append(createLi)
+
+    // Clears inputfield after submit
+    formText.value = ""
+}
+
+formE1.addEventListener("submit", addTodo)

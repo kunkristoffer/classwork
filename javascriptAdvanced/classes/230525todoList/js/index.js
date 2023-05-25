8b0336ea5138047855a2d4e1bb273c61ac61e5e1
kristofferfh <kristofferfh.kodehode@gmail.com>
2023-05-25 10:48:41 +0200

    uploading classworks

---
+++ javascriptAdvanced/classes/230525todoList/js/index.js
@@ -0,0 +1,10 @@
+"use strict"
+
+// import {*} from "./data.js"
+// import {*} from "./render.js"
+
+// define DOM input/output
+const htmlInput = document.getElementById("input")
+const htmlOutput = document.getElementById("output")
+
+// run on page load
